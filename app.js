e5edf4eea4d7528db4b8b6ea3fe0005e2c95811b
Parag Chauhan <iamparag121298@gmail.com>
2023-03-27 06:07:59 +0530

    Update app.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.get((req,res)=>{
 
 app.use(bodyParser.json())
 app.use(cors());
-const PORT=process.env.PORT || 3000
+const PORT=process.env.PORT || 3000 || 8080
 
 app.listen(PORT,()=>{
     console.log('server has started on 3000')
